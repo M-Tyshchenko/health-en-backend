f9c0dcaa6bef6c834945cfb6d673a86be391550f
Mariia Tyshchenko <122963239+M-Tyshchenko@users.noreply.github.com>
2023-12-13 09:29:34 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const path = require("node:path");
+require("dotenv").config();
+
+const authRouter = require("./routes/api/auth");
+const contactsRouter = require("./routes/api/contacts");
+
+const app = express();
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use("/avatars", express.static(path.join(__dirname, "public/avatars")));
+app.use("/users", authRouter);
+app.use("/api/contacts", contactsRouter);
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((err, req, res, next) => {
+    
+  res.status(500).json({ message: err.message });
+});
+
+module.exports = app;
---
+++ routes/api/auth.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const UserController = require("../../controllers/auth");
+
+const { authenticate, upload } = require("../../middlewares");
+
+const jsonParser = express.json();
+
+const router = express.Router();
+
+router.post("/register", jsonParser, UserController.register);
+
+router.post("/login", jsonParser, UserController.login);
+
+router.get("/current", authenticate, UserController.current);
+
+router.post("/logout", authenticate, UserController.logout);
+
+router.get("/verify/:verificationToken", UserController.verify);
+
+router.post("/verify", jsonParser, UserController.resendVerify);
+
+router.patch("/", authenticate, jsonParser, UserController.updateStatusUser);
+
+router.patch("/avatars", authenticate, upload.single("avatar"), UserController.updateAvatar);
+
+module.exports = router;
