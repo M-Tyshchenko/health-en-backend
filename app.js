07147d52fcc64edbb0616201e53cf233a9d79df8
Mariia Tyshchenko <m.tish2406@gmail.com>
2023-12-13 16:34:40 +0200

    add elastic key, fixed auth path in app

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use("api/auth", authRouter);
+app.use("/api/auth", authRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
