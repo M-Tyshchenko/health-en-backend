f8a363d3557c227765ab6e8a2e3cf3b0d62d75f1
M-Tyshchenko <m.tish2406@gmail.com>
2023-12-23 23:51:17 +0200

    Update handleMongooseError.js
    add error.message for 409 status

--- helpers/handleMongooseError.js
+++ helpers/handleMongooseError.js
@@ -2,6 +2,11 @@ const handleMongooseError = async (error, __, next) => {
   const { name, code } = error;
   const status = name === "MongoServerError" && code === 11000 ? 409 : 400;
   error.status = status;
+
+  if (error.status === 409) {
+    error.message = "Email in use";
+  }
+
   next();
 };
 
